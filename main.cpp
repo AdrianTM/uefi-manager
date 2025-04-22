8c2773bd23cbd38d724bb16024744110c3c025bb
Adrian <adrian@mxlinux.org>
2025-04-21 21:12:40 -0400

    Update to Qt6

--- main.cpp
+++ main.cpp
@@ -56,12 +56,12 @@ int main(int argc, char *argv[])
     QApplication::setApplicationDisplayName(QObject::tr("UEFI Manager"));
 
     QTranslator qtTran;
-    if (qtTran.load("qt_" + QLocale::system().name(), QLibraryInfo::location(QLibraryInfo::TranslationsPath))) {
+    if (qtTran.load("qt_" + QLocale::system().name(), QLibraryInfo::path(QLibraryInfo::TranslationsPath))) {
         QApplication::installTranslator(&qtTran);
     }
 
     QTranslator qtBaseTran;
-    if (qtBaseTran.load("qtbase_" + QLocale::system().name(), QLibraryInfo::location(QLibraryInfo::TranslationsPath))) {
+    if (qtBaseTran.load("qtbase_" + QLocale::system().name(), QLibraryInfo::path(QLibraryInfo::TranslationsPath))) {
         QApplication::installTranslator(&qtBaseTran);
     }
 
